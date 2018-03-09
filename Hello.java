abe99ad72526c0c2c79b9ee350b29a3cb068e877
hannykee <hchw1904@gmail.com>
2018-03-09 12:39:57 +0900

    java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.applet.*;
+
+public class Hello extends Applet
+{
+	public void paint(Graphics g) 
+	{
+		g.drawString("Hello World!",25,25);
+	}
+}
---
+++ StringBufferTest.java
@@ -0,0 +1,10 @@
+class StringBufferTest 
+{
+	public static void main(String[] args) 
+	{
+		StringBuffer buf = new StringBuffer();
+		buf.append("hannykim  ");
+		buf.append("in Java World");
+		System.out.println(buf.toString());
+	}
+}
